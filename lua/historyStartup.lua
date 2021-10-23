771a5526e9676b568b190980b10c92bc9d77236a
iaso2h <iaso2h@outlook.com>
2021-10-23 17:39:55 +0800

    Fix stratup keymap

--- lua/historyStartup.lua
+++ lua/historyStartup.lua
@@ -1,13 +1,16 @@
 -- File: historyStartup
 -- Author: iaso2h
 -- Description: Startup page with oldfiles
--- Version: 0.0.4
+-- Version: 0.0.5
 -- Last Modified: 2021-09-16
 -- TODO: mapping
 local api = vim.api
 local fn  = vim.fn
 local cmd = vim.cmd
-local M   = {}
+local M   = {
+    curBuf = nil,
+    lastBuf = nil,
+}
 
 
 local lines = {"< New Buffer >"}
@@ -27,8 +30,13 @@ M.display = function()
 
     if vim.bo.filetype == "HistoryStartup" then return end
 
-    -- -- TODO: alert warning
-    -- if vim.fn.line2byte("$") ~= -1 then return end
+    -- Save last opened buffer and restore it after open a new buffer
+    if vim.bo.buftype == "" and api.nvim_buf_get_name(0) ~= "" then
+        M.lastBuf = api.nvim_get_current_buf()
+    else
+        M.lastBuf = nil
+    end
+
 
     curWin = api.nvim_get_current_win()
     if api.nvim_buf_is_valid(1) then
@@ -55,29 +63,31 @@ M.display = function()
 
     api.nvim_buf_set_option(M.curBuf, "modifiable", false)
 
-    for _, key in ipairs({"o", "s", "v", "<cr>"}) do
+    for _, key in ipairs({"o", "<C-s>", "<C-v>", "<CR>"}) do
         api.nvim_buf_set_keymap(
             M.curBuf,
             "n",
             key,
-            string.format([=[:lua require("historyStartup").do_map([[%s]])<cr>]=], key),
+            string.format([=[:lua require("historyStartup").execMap([[%s]])<CR>]=], string.gsub(key, [[<]], [[<lt>]])),
             {silent = true}
         )
     end
 
 end
 
-M.do_map = function(key)
+M.execMap = function(key)
     local target = lines[api.nvim_win_get_cursor(0)[1]]
 
     if target == "< New Buffer >" then
-        cmd("enew!")
-    elseif key == "o" or key == "<cr>" then
-        cmd("edit! " .. target)
-    elseif key == "s" then
-        cmd("split! " .. target)
-    elseif key == "v" then
-        cmd("vsplit! " .. target)
+        cmd("enew")
+    elseif key == "o" or string.lower(key) == "<cr>" then
+        cmd("edit " .. target)
+    elseif string.lower(key) == "<c-s>" then
+        api.nvim_win_set_buf(curWin, M.lastBuf)
+        cmd("split " .. target)
+    elseif string.lower(key) == "<c-v>" then
+        api.nvim_win_set_buf(curWin, M.lastBuf)
+        cmd("vsplit " .. target)
     end
     M.deleteBuf()
 end
