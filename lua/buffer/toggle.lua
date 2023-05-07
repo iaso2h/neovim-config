716b440f0479b3cabb451c81ce7e41240cc2e9a9
iaso2h <iaso2h@outlook.com>
2023-05-07 11:03:40 +0800

    refactor(toggle): handle terminal and quickfix

---
+++ lua/buffer/toggle.lua
@@ -0,0 +1,77 @@
+-- File: toggle.lua
+-- Author: iaso2h
+-- Description: Toggle terminal like VS Code does
+-- Last Modified: 05/07/2023 Sun
+-- Version: 0.0.6
+
+local quickfix = function(closeChk) -- {{{
+    -- Close the current window if it's a quickfix window
+    if vim.bo.buftype == "quickfix" then
+        return vim.cmd [[cclose]]
+    end
+
+    -- Toggle on
+    local winInfos = vim.fn.getwininfo()
+    for _, winInfo in ipairs(winInfos) do
+        if winInfo["quickfix"] == 1 then
+            if closeChk then
+                return vim.api.nvim_win_close(winInfo.winid, false)
+            else
+                return vim.api.nvim_set_current_win(winInfo.winid)
+            end
+        end
+    end
+
+    if closeChk then
+        return
+    end
+
+    -- Fallback
+    vim.cmd "copen"
+end -- }}}
+local terminal = function(closeChk) -- {{{
+    local winInfos = vim.fn.getwininfo()
+
+    if vim.bo.buftype ~= "terminal" then
+        for _, winInfo in ipairs(winInfos) do
+            if winInfo["terminal"] == 1 then
+                if closeChk then
+                    return vim.api.nvim_win_close(winInfo.winid, false)
+                else
+                    return vim.api.nvim_set_current_win(winInfo.winid)
+                end
+            end
+        end
+
+        if closeChk then
+            return
+        end
+
+        -- Search terminal in hidden buffer list
+        for exCmd in ipairs{"R", "F"} do
+            local output = vim.api.nvim_exec2("ls! " .. exCmd, {output = true}).output
+            if output ~= "" then
+                local cmdLines = vim.split(output, "\n", {plain = true})
+                for _, cmdLine in ipairs(cmdLines) do
+                    local bufNr = string.match(cmdLine, "(%d+).* ['\"]term://")
+                    if bufNr then
+                        return vim.cmd("split | buffer " .. bufNr)
+                    end
+                end
+            end
+        end
+
+        vim.cmd "split"
+        vim.cmd "terminal"
+    else
+        vim.api.nvim_win_close(0, false)
+    end
+end -- }}}
+
+return function(type, closeChk)
+    if type == "quickfix" then
+        return quickfix(closeChk)
+    elseif type == "terminal" then
+        return terminal(closeChk)
+    end
+end
