2b6e4baa3bce8641c830117f05c91114c6fa08e7
iaso2h <iaso2h@outlook.com>
2025-03-04 21:37:37 +0800

    chore: updates

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -43,7 +43,8 @@ return function()
         -- Mappings
 
         bmap(bufNr, "n", [[K]], function() vim.lsp.buf.hover {border = _G._float_win_border} end, "Documentation")
-        bmap(bufNr, "n", [[<C-f>O]], [[<CMD>lua require('telescope.builtin').lsp_workspace_symbols()<CR>]], {"silent"}, "LSP workspace symbols")
+        bmap(bufNr, "n", [[<C-f>o]], [[<CMD>lua require('telescope.builtin').lsp_workspace_symbols()<CR>]], {"silent"}, "LSP workspace symbols")
+        bmap(bufNr, "n", [[<C-f><C-o>]], [[<CMD>lua require('telescope.builtin').lsp_workspace_symbols()<CR>]], {"silent"}, "LSP workspace symbols")
 
         bmap(bufNr, "n", [[gd]], function()
             vim.lsp.buf.definition { on_list = conciseQuifix }
