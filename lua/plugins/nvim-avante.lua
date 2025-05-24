ca579f552f9376be040c50b23500b13082077136
iaso2h <iaso2h@outlook.com>
2025-05-24 23:50:40 +0800

    chore: disable ai auto-completion in input mode

--- lua/plugins/nvim-avante.lua
+++ lua/plugins/nvim-avante.lua
@@ -18,7 +18,7 @@ return function()
             },
         },
     behaviour = {
-        auto_suggestions = true, -- Experimental stage
+        auto_suggestions = false, -- Experimental stage
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = false,
